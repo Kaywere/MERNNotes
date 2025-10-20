4bdecbb84fafe4888bba081d1de3098d35abc723
Soss <l56eerl@gmail.com>
2025-10-20 10:08:33 +0300

    Update the API

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://172.16.15.2:5011/api",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5011/api",
 });
 
 export default api;
