2bac324087eb79c4e9db2d7d43586c3424df0a68
Soss <l56eerl@gmail.com>
2025-10-20 10:47:20 +0300

    Fixed the CORS origin

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL || 'http://192.168.1.51:5011/api'
 });
 
 export default api;
